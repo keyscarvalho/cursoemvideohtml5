379a49002213827ba4fd1f5d713089dceec2ee8e
Keylla Souza de Carvalho <kscarvalho10@gmail.com>
2024-10-04 16:58:40 -0300

    meus downloads

---
+++ pasta download/DESAFIO 02.py
@@ -0,0 +1,4 @@
+dia = input('Qual o dia de nascimento? ')
+mês = input('Qual o mês de nascimento? ')
+ano = input('Qual o ano de nascimento? ')
+print('Você nasceu no dia ', dia,('de'),mês,('de'),ano, ('. Correto? '))
